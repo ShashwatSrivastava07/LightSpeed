b0c5bcbd5c4d3878dad39cf5265dbcb219672e37
Shashwat Srivastava <shashwatsrivastavait30@gmail.com>
2025-04-08 17:59:14 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <HeaderNav />
+  </div>
+</template>
+
+<script>
+
+  import HeaderNav from './components/HeaderNav.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      HeaderNav,
+    }
+  }
+</script>
+
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
